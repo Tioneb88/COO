3e521ede304ad89e47239abae14b4f6f58f7bedd
ArnaudC08 <arnaud.claes@student.uclouvain.be>
2018-04-28 16:56:14 +0200

    correction bug spinner

--- MiniPoll/app/src/main/java/lsinf1225/mini_poll/activity/LoginActivity.java
+++ MiniPoll/app/src/main/java/lsinf1225/mini_poll/activity/LoginActivity.java
@@ -3,6 +3,7 @@ package lsinf1225.mini_poll.activity;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.inputmethod.EditorInfo;
